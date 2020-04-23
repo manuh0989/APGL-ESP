52aba4d4f7c1b7c22c3d7ff829fbf243629354e0
Ignacio Sanchez <manuh0989@gmail.com>
2020-04-23 13:16:27 -0500

    prueba

--- resources/views/layouts/mail.blade.php
+++ resources/views/layouts/mail.blade.php
@@ -31,7 +31,6 @@
             </div>
         </div>
 
-
         <script src="https://code.jquery.com/jquery-3.4.1.min.js" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js" crossorigin="anonymous"></script>
         
